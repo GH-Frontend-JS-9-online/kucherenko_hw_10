31d38962b746c1f18ada3e8a6bc226b6596759f2
FriskyLuckyBoy <sanyamorre@gmail.com>
2019-12-31 12:08:32 +0300

    ok

--- js/currency-converter.js
+++ js/currency-converter.js
@@ -2,36 +2,35 @@
 // const axios = require('axios');
 
 const getExchangeRate = async (fromCurrency, toCurrency) => {
-    try {
-      const response = await axios.get('http://www.apilayer.net/api/live?access_key=af9e2ea885cdf255e17836c9c1e97c8b')
+  try {
+    const response = await axios.get('http://www.apilayer.net/api/live?access_key=af9e2ea885cdf255e17836c9c1e97c8b')
 
-      const rate = response.data.quotes;
-      const baseCurrency = response.data.source;
-      const usd = 1 / rate[`${baseCurrency}${fromCurrency}`];
-      const exchangeRate = usd * rate[`${baseCurrency}${toCurrency}`];
+    const rate = response.data.quotes;
+    const baseCurrency = response.data.source;
+    const usd = 1 / rate[`${baseCurrency}${fromCurrency}`];
+    const exchangeRate = usd * rate[`${baseCurrency}${toCurrency}`];
   
-      return exchangeRate;
-    } catch (error) {
-      throw new Error(`Unable to get currency ${fromCurrency} and ${toCurrency}`);
-    }
+    return exchangeRate;
+  } catch (error) {
+    throw new Error(`Unable to get currency ${fromCurrency} and ${toCurrency}`);
+  }
 };
   
 const getCountries = async (currencyCode) => {
-    try {
-      const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-  
-      return response.data.map(country => country.name);
-    } catch (error) {
-      throw new Error(`Unable to get countries that use ${currencyCode}`);
-    }
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+    return response.data.map(country => country.name);
+  } catch (error) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`);
+  }
 };
   
 const convertCurrency = async (fromCurrency, toCurrency, amount) => {
-    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
-    const countries = await getCountries(toCurrency);
-    const convertedAmount = (amount * exchangeRate).toFixed(2);
+  const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+  const countries = await getCountries(toCurrency);
+  const convertedAmount = (amount * exchangeRate).toFixed(2);
   
-    return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
 };
   
 
@@ -58,12 +57,10 @@ function result() {
     let amount = document.getElementById('amount').value;
     
     convertCurrency(fromCurrency, toCurrency, amount).then((message) => {
-        console.log(message);
       document.getElementById('result').innerHTML = message;
     }).catch((error) => {
       console.log(error.message);
     })
   }
 }
-
 result();
